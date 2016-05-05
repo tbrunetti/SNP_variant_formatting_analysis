25b71556b225611c82ce387f2c5ce8bc4829f83b
tbrunetti <tonya.brunetti@colorado.edu>
2016-05-05 14:20:38 -0500

    formats files for FunSeq

---
+++ formatBED_funseq_input.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+#sys.argv[1] is a list of exact file names to reformat, one file name per line
+def formatBed_funseq(pathToVCF):
+
+	with open(sys.argv[1]) as input:
+		for line in input:
+			os.chdir(pathToVCF)
+			with open(line.rstrip('\n')) as input:
+				os.chdir(pathToOutput)
+				f=open(str(line[:-4])+'-'+'input-funseq.txt', 'w')
+				for variant in input:
+					variant=variant.split('\t')
+					print variant
+					f.write(str(variant[0])+'\t'+str((float(variant[1])-1))+'\t'+str(float(variant[1]))+'\t'+str(variant[2])+'\t'+str(variant[3])+'\n')
+
+
+if __name__=='__main__':
+	#this is path to 'patient_files_offtargets_removed' folder with no headers
+	pathToVCF='/home/tonya/pan_can_project/panData_UCretro_DNA_mutations/patient-files-offtargets-removed'
+	pathToOutput='/home/tonya/pan_can_project/panData_UCretro_DNA_mutations/bedFormatted_files_funseq'
+	formatBed_funseq(pathToVCF);
