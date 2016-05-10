79ea23fb20e468f447fdb29aab2877a7a61fe21b
tbrunetti <tonya.brunetti@colorado.edu>
2016-05-10 16:55:02 -0500

    added clustering based on CADD scores

--- cluster-mutations.py
+++ cluster-mutations.py
@@ -1,5 +1,6 @@
 import sys
 from collections import Counter
+from operator import itemgetter
 import pandas
 import seaborn as sns
 
@@ -9,17 +10,25 @@ def makeCluster():
 	#key=patID, value=list of predicted protein function loss
 	patientMutations={}
 	#stores list of all genes (including redundant), predicted for loss of protein function
+	patientMutationsCADD={}
 	numberDiffentGenes=[]
 	with open(sys.argv[1]) as input:
 		for line in input:
 			line=line.split('\t')
 			#line[3] is the name of the mutated gene
-			numberDiffentGenes.append(line[3])
+			numberDiffentGenes.append(line[7])
 			#line[0] is the patientID
 			if line[0] in patientMutations:
-				patientMutations[line[0]]=patientMutations[line[0]]+[line[3]]
+				patientMutations[line[0]]=patientMutations[line[0]]+[line[7]]
+				patientMutationsCADD[line[0]]=patientMutationsCADD[line[0]]+[(str(line[7]),float(line[16].rstrip('\n')))]
 			else:
-				patientMutations[line[0]]=[line[3]]
+				patientMutations[line[0]]=[line[7]]
+				patientMutationsCADD[line[0]]=[(str(line[7]),float(line[16].rstrip('\n')))]
+
+	for key in patientMutationsCADD:
+		print patientMutationsCADD[key]
+		#print Counter(patientMutationsCADD[key][0] for key in patientMutationsCADD)
+
 
 	#prints the number of unique genes seen			
 	print len(set(numberDiffentGenes))
@@ -29,7 +38,7 @@ def makeCluster():
 	#removes all genes that only appear once, to help reduce noise in data set
 	removeSingletons={}
 	for key in totalGenesMuts:
-		if totalGenesMuts[key]<=2:
+		if totalGenesMuts[key]<=7:
 			continue;
 		else:
 			removeSingletons[key]=totalGenesMuts[key]
@@ -53,13 +62,68 @@ def makeCluster():
 	dataframe=pandas.DataFrame(listOfpatsWithMutCounts, columns=['patientID', 'gene_mutated', 'frequency'])
 	#print dataframe
 	dataframe=dataframe.pivot("patientID", "gene_mutated", "frequency")
-	clusterData=sns.clustermap(dataframe, method='single', metric='euclidean')
+	clusterData=sns.clustermap(dataframe, method='weighted', metric='euclidean')
+	sns.plt.show()
+
+	#cluster data similar to above, but hue is now CADD score
+	listOfpatsCADD=[]
+	for key in patientMutationsCADD:
+		for i in range(0, len(genesToConsider)):
+			#checks if patient had gene mutation, if not, the bottom else statment will give it a CADD of zero
+			if genesToConsider[i] in [patientMutationsCADD[key][tuples][0] for tuples in range(len(patientMutationsCADD[key]))]:
+				caddScore=[patientMutationsCADD[key][tuples][1] for tuples in range(len(patientMutationsCADD[key])) if patientMutationsCADD[key][tuples][0]==genesToConsider[i]]
+				#if there are multiple mutations in the same gene for a patient, take the highest CADD score
+				if len(caddScore)>1:
+					entryCADD=[str(key), genesToConsider[i], max(caddScore)]
+					print entryCADD
+					listOfpatsCADD.append(entryCADD)
+				else:
+					entryCADD=[str(key), genesToConsider[i], caddScore[0]]
+					listOfpatsCADD.append(entryCADD)
+			else:
+				entryCADD=[str(key), genesToConsider[i], 0]
+				listOfpatsCADD.append(entryCADD)
+	#print listOfpatsCADD
+	dataframeCADD=pandas.DataFrame(listOfpatsCADD, columns=['patientID', 'gene_mutated', 'CADD_score'])
+	dataframeCADD=dataframeCADD.pivot('patientID', 'gene_mutated', 'CADD_score')
+	clusterCADDdata=sns.clustermap(dataframeCADD, method='single', metric='euclidean', cmap='hot_r')
 	sns.plt.show()
 
 def input_ROCKclustering_in_R():
+	#key=patID, value=list of predicted protein function loss
+	patientMutations={}
+	#stores list of all genes (including redundant), predicted for loss of protein function
+	numberDiffentGenes=[]
+	with open(sys.argv[1]) as input:
+		for line in input:
+			line=line.split('\t')
+			#line[7] is the name of the mutated gene
+			numberDiffentGenes.append(line[7])
+			#line[0] is the patientID
+			if line[0] in patientMutations:
+				patientMutations[line[0]]=patientMutations[line[0]]+[line[7]]
+			else:
+				patientMutations[line[0]]=[line[7]]
 
-	print "unfinished"
+	#prints the number of unique genes seen			
+	print len(set(numberDiffentGenes))
+	#counts the number of times a gene is predicted to be deleterious in predicted protein function
+	totalGenesMuts=dict(Counter(numberDiffentGenes))
 
+	#removes all genes that only appear once or set int, to help reduce noise in data set
+	removeSingletons={}
+	for key in totalGenesMuts:
+		if totalGenesMuts[key]<=1:
+			continue;
+		else:
+			removeSingletons[key]=totalGenesMuts[key]
+
+	print len(removeSingletons) 
+	f=open('gene_for_downstream_analysis.txt', 'w')
+	genesToConsider=[key for key in removeSingletons]
+	for i in genesToConsider:
+		f.write(str(genesToConsider[i])+'\n')
 
 if __name__=='__main__':
 	makeCluster();
+	#input_ROCKclustering_in_R();
