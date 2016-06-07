370e5e9dee11610d42bbec795f8e0823e1a9b64d
tbrunetti <tonya.brunetti@colorado.edu>
2016-06-07 11:33:46 -0500

    added more stats, not well documented yet, on TODO list

--- variantStats.py
+++ variantStats.py
@@ -5,7 +5,10 @@ import plotly
 import plotly.plotly as plotly
 import plotly.graph_objs as go
 import plotly.offline
- 
+import seaborn as sns
+import pandas
+from collections import Counter
+
 #plotly.sign_in('tbrunetti', '72r07ya8dr')
 
 
@@ -50,7 +53,7 @@ def makeBoxplot(totalVarsPerPatient):
 
 #sys.argv[1]=tab-delimited file, each line is one variant of a patient (PDAC)
 #sys.argv[2]=tab-delimited file, each line is one variant of a patient (nonPDAC)
-#first column is patientID, 8th column is mutation type, rest of columns need to have space hold
+#first column is patientID, 8th column is gene mutation, rest of columns need to have space hold
 def deleteriousVariants():
 	patientVariantsPDAC={}
 	with open(sys.argv[1]) as input:
@@ -104,9 +107,100 @@ def deleteriousVariants():
 	print "The median number of variants in a non-PDAC patient is "+ str(np.median(numberOfDelMutationsNonPDAC))
 	
 
+#sys.argv[1]=tab-delimited file, each line is one variant of a patient
+#first column is patientID, 8th column is gene mutation and 9th is mutation type, rest of columns need to have space hold
+#return a graph of the number of patients that have a particular mutation type in a given gene,
+#also prints out total number of patients and the NON-SYNONYMOUS mutation change is available
+def breakdown_of_specific_deleterious_vars():
+	geneInterest='SMAD4'
+	mutationTypes=[]
+	patientMutation={}
+	patientMutNonSyn={}
+	with open(sys.argv[1]) as input:
+		headers=next(input)
+		for line in input:
+			line=line.split('\t')
+
+			if line[7]==geneInterest:
+				mutationTypes.append(line[8])
+				if line[0] in patientMutation:
+					patientMutation[line[0]]=patientMutation[line[0]]+[line[8]]
+				else:
+					patientMutation[line[0]]=[line[8]]
+
+			if line[7]==geneInterest and line[8]=='NON_SYNONYMOUS_CODING':
+				if line[0] in patientMutNonSyn:
+					patientMutNonSyn[line[0]]=patientMutNonSyn[line[0]]+[line[9]]
+				else:
+					patientMutNonSyn[line[0]]=[line[9]]	
+
+	print patientMutation
+	print patientMutNonSyn
+	print len(patientMutation)
+
+	dataframe=pandas.DataFrame(mutationTypes, columns=['mutation type'])
 	
+	with sns.plotting_context("notebook", font_scale=2):
+		sns.countplot(x='mutation type', data=dataframe)
+		sns.plt.show()
+
+def breakdown_of_deleterious_vars_between_groups():
+	pdacMuts=[]
+	nonPDACmuts=[]
+	patientCompleteProfilePDAC={}
+	patientCompleteProfileNonPDAC={}
+	with open(sys.argv[1]) as input:
+		headers=next(input)
+		for line in input:
+			line=line.split('\t')
+			if line[0] in patientCompleteProfilePDAC:
+				patientCompleteProfilePDAC[line[0]]=patientCompleteProfilePDAC[line[0]]+[line[7]]
+			if line[0] not in patientCompleteProfilePDAC:
+				patientCompleteProfilePDAC[line[0]]=[line[7]]
+
+	with open(sys.argv[2]) as input:
+		headers=next(input)
+		for line in input:
+			line=line.split('\t')
+			if line[0] in patientCompleteProfileNonPDAC:
+				patientCompleteProfileNonPDAC[line[0]]=patientCompleteProfileNonPDAC[line[0]]+[line[7]]
+			if line[0] not in patientCompleteProfileNonPDAC:
+				patientCompleteProfileNonPDAC[line[0]]=[line[7]]
+
+	#print patientCompleteProfilePDAC
+	#print patientCompleteProfileNonPDAC
+	temp1=[]
+	temp2=[]
+	for key in patientCompleteProfilePDAC:
+		temp1=temp1+list(set(patientCompleteProfilePDAC[key]))
+	for key in patientCompleteProfileNonPDAC:
+		temp2=temp2+list(set(patientCompleteProfileNonPDAC[key]))
+
+	#print len(patientCompleteProfilePDAC)
+	#print len(patientCompleteProfileNonPDAC)
+	#print Counter(temp1).most_common(10)[i][j]
+	#print Counter(temp2).most_common(10)
+
+	forDataFrame=[]
+	for i in range(0, 10):
+		forDataFrame.append([Counter(temp1).most_common(10)[i][0], Counter(temp1).most_common(10)[i][1], 'PDAC'])
+		forDataFrame.append([Counter(temp2).most_common(10)[i][0], Counter(temp2).most_common(10)[i][1], 'NON-PDAC'])
+
+	dataframe=pandas.DataFrame(forDataFrame, columns=['gene', 'frequency', 'diagnosis'])
+	print dataframe
+
+	geneOrder=['KRAS', 'TP53', 'GNAQ', 'CHEK2', 'SMAD4', 'KMT2C', 'PRDM16', 'LTBP2', 'NOTCH3', 'OBSCN', 'PTGS1', 'TBX22', 'PRSS1', 'RICTOR', 'TSC2', 'OPTN']
+	with sns.plotting_context("notebook", font_scale=1.5):
+		sns.barplot(x='gene', y='frequency', hue='diagnosis', data=dataframe, order=geneOrder)
+	#sns.set_context("poster", font_scale=5)
+		sns.set(font_scale=2.0)
+		sns.plt.legend(loc='upper right')
+		sns.plt.show()
+
 if __name__=='__main__':
 	directoryPatientVariants='/home/tonya/pan_can_project/panData_UCprospective_DNA_mutations/variant-viewer-files-offtargets-removed'
 	#totalVarsPerPatient=variantStats(directoryPatientVariants);
 	#makeBoxplot(totalVarsPerPatient);
-	deleteriousVariants();
+	#deleteriousVariants();
+	breakdown_of_specific_deleterious_vars();
+	#breakdown_of_deleterious_vars_between_groups();
